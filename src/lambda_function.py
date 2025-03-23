b9158669293d8cdf3a1b0691437c86a4038cb081
yingwang <wangying9@gmail.com>
2025-03-23 11:20:01 -0700

    create lambda and deployment files

---
+++ src/lambda_function.py
@@ -0,0 +1,8 @@
+# src/lambda_function.py
+
+def lambda_handler(event, context):
+    # Just a simple response for this example
+    return {
+        'statusCode': 200,
+        'body': 'Hello from Lambda!'
+    }
