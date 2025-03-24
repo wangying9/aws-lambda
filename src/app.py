de269e60715bd5ca359c7b0df4b677539b0ba0cc
yingwang <wangying9@gmail.com>
2025-03-23 20:38:44 -0700

    dependance

--- src/app.py
+++ src/app.py
@@ -1,8 +1,8 @@
 # src/lambda_function.py
-
+import requests
 def lambda_handler(event, context):
     # Just a simple response for this example
     return {
         'statusCode': 200,
-        'body': 'Hello from Lambda!3355'
+        'body': 'Hello from Lambda!33556'
     }
